cf79b533a55077364906bb4e7afb6b19e66956df
PJH <thetrax5@naver.com>
2018-08-07 10:35:26 +0900

    findviewbyid를 onCreate로 모아서 효율성 개선

--- app/src/main/java/com/example/it/survivecoding/MainActivity.java
+++ app/src/main/java/com/example/it/survivecoding/MainActivity.java
@@ -12,12 +12,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = MainActivity.class.getSimpleName();
     private final int price= 4500;
     private int mQuantity = 0;
+    private TextView quantityTextView;
+    private TextView mPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView quantityTextView =
+                findViewById(R.id.quantity_text_view);
+        mPriceTextView = findViewById(R.id.price_text_view);
         findViewById(R.id.increaseBtn).setOnClickListener(this);
         findViewById(R.id.decreaseBtn).setOnClickListener(this);
     }
@@ -31,15 +36,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         displayMsg(priceMsg);
     }
     private void display(int number){
-        TextView quantityTextView =
-                findViewById(R.id.quantity_text_view);
         quantityTextView.setText(""+number);
     }
+
     private void displayPrice(int number){
-        TextView priceTextView
-                = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        mPriceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
         //$를 해당 국가의 화폐 단위로 변경
+    }
+
+    private void displayMsg(String msg) {
+        mPriceTextView.setText(msg);
     }
 
     @Override
@@ -64,11 +70,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void Increase() {
         mQuantity++;
-    }
-
-    private void displayMsg(String msg) {
-        TextView priceTextView
-                = findViewById(R.id.price_text_view);
-        priceTextView.setText(msg);
     }
 }
